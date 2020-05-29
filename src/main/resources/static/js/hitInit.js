3255ae7ea4b8419ac92536028bb72330395c9449
htp <84121599@qq.com>
2020-05-29 17:12:34 +0800

    更新了教师学生界面；完成了大部分逻辑

---
+++ src/main/resources/static/js/hitInit.js
@@ -0,0 +1,6 @@
+(function(){
+	var hit=document.getElementById("infoHit");
+	if(hit!=null){
+		if(hit.innerHTML == "0")hit.style.visibility="hidden";
+	}
+})()
--- src/main/resources/static/js/teacher.js
+++ src/main/resources/static/js/teacher.js
@@ -1,7 +1,19 @@
+
+/*创建班级窗口*/
+var buildclassWindow=document.getElementById("buildClassWindow")
 function clickToBuildClass() {
-    window.location.href="buildClass"
+    // window.location.href="buildClass"
 }
 
+function clickCloseWindow(){
+	
+}
+
+function clickCloseWindow(){
+	
+}
+
+/**/
 function classInfoClick(c_id) {
     console.log(c_id);
     window.location.href="classInfo/"+c_id;
@@ -19,11 +31,41 @@ function classMessageClick(c_id) {
     window.location.href="classMessage/"+c_id;
 }
 
+/*以下为页面管理*/
+var firstPage=document.getElementById("firstPage");
+var previousPage=document.getElementById("previousPage");
+var nextPage=document.getElementById("nextPage");
+var lastPage=document.getElementById("lastPage");
+var nowPage=document.getElementById("nowPage");
+var allPage=document.getElementById("allPage");
+(function initPage(){
+	console.log(nowPage.innerHTML);
+	console.log(allPage.innerHTML);
+	
+	if(nowPage.innerHTML==allPage.innerHTML){
+		nextPage.style.visibility="hidden"
+		lastPage.style.visibility="hidden"
+	}
+	
+	if(nowPage.innerHTML==1){
+		console.log(firstPage.style)
+		firstPage.style.visibility="hidden"
+		previousPage.style.visibility="hidden"
+	}
+})()
 
+function firstClick(){
+	window.location.href="../index_teacher/1";
+}
 
-// function classInfoClick() {
-//
-// }
-// function classInfoClick() {
-//
-// }
+function lastClick(){
+	window.location.href="../index_teacher/"+allPage.innerHTML;
+}
+
+function previousClick(){
+	window.location.href="../index_teacher/"+(Number(nowPage.innerHTML)-1);
+}
+
+function nextClick(){
+	window.location.href="../index_teacher/"+(Number(nowPage.innerHTML)+1);
+}
